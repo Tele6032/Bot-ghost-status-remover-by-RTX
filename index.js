6713576442cbdeeaf7dbf0bd96929c7015fc8618
Tele6032 <166168453+Tele6032@users.noreply.github.com>
2024-04-05 21:09:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Regarde la version 1.0","Regarde le développement avanvcé par Tele6032"];
 
 
 let currentIndex = 0;
